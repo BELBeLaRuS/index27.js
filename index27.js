77638340dc67ab6529f27d8c5d35f9a3a74d532c
BELBeLaRuS <157610529+BELBeLaRuS@users.noreply.github.com>
2024-12-08 19:41:10 +0300

    Add files via upload

---
+++ index27.js
@@ -0,0 +1,231 @@
+//alert('Осторожно, массивы!');
+
+// let mass = [];
+
+// Massive begins from 0 to infinity!!!
+// mass[0] = 1;
+// mass[1] = 2;
+// mass[1000] = 34;
+// console.log(mass);
+
+// let masss = [1, 67, 'che pisat', 412];
+
+// let size = masss.length;
+
+//console.log(size);
+
+// console.log(masss);
+// console.log(masss.reverse());
+
+// let m1 = [];
+
+// m1[19] = 0;
+// m1 = m1.fill(20);
+
+// console.log(m1);
+
+// let str = 'hello world!';
+// console.dir(str);
+
+// let phone = {
+//     size: [10, 30, 1.5],
+//     num: '+375 44 231 42 43',
+//     os: {
+//         name: 'Android',
+//         version: '14.1',
+//         lastupdate: '15.03.1941'
+//     },
+//     // callnumberr(callnumber) {
+//     //     console.log(` call at ${callnumber}`);
+//     // },
+//     callnumberr: function(callnumber){
+//         console.log(`Call at ${callnumber}`);
+//     }
+// };
+
+// phone.callnumberr('+375 44 534 23 43');
+// console.log(phone.os.lastupdate);
+// console.log(phone.os.name);
+
+
+// function getRandomInt(max) {
+//     Math.floor(Math.random() * max);
+// }
+
+// var num1 = prompt('input first number: ');
+// var num2 = prompt('input second number: ');
+
+
+// + - * ^
+// if (num1 + num2) {
+//     let res = num1 + num2;
+//     console.log(res);
+// }
+
+// if (num1 - num2) {
+//     let res2 = num1 - num2;
+//     console.log(res2);
+// }
+
+// if (num1 * num2) {
+//     let res3 = num1 * num2;
+//     console.log(res3);
+// }
+
+// if (num1 ** num2) {
+//     let res5 = num1 ** num2;
+//     console.log(res5);
+// }
+
+
+
+//1.
+// // var num1 = prompt('Input number: ');
+    
+// // if(num1 > 0) {
+// //     console.log(num1 + ': this is positive number. ');
+// // }
+// // else{
+// //     console.log(num1 + ': this is negative number. ');
+// // }
+// // if (num1 == 0){
+// //     console.log(num1 + ': this is zero. ');
+// // }
+
+
+//2.
+// var num1 = prompt('Input your age: ');
+
+// if (num1 > 0 || num1 < 120) {
+//     console.log('Good age. ');
+// }
+
+// if (num1 < 0 || num1 > 120) {
+//     console.log('Your age is incorrect. ');
+// }
+
+
+//3.
+// var num1 = prompt('Input number: ');
+
+// if(num1 > 0) {
+//     console.log(num1);
+// }
+// else{
+//     res = Math.abs(num1);
+//     console.log(res);
+// }
+
+
+//4.
+// var sec = prompt('Input seconds: ');
+// var min = prompt('Input minutes: ');
+// var hour = prompt('Input hours: ');
+
+// if(sec > 60 || sec < 0 ) {
+//     console.log('Your time is incorrect :( ');
+// }
+
+// else {
+//     console.log('your time is write :( ');
+// }
+
+
+// if(min > 60 || min < 0 ) {
+//     console.log('Your time is incorrect :( ');
+// }
+
+// else {
+//     console.log('your time is write :( ');
+// }
+
+
+// if(hour > 24 || hour < 0 ) {
+//     console.log('Your time is incorrect :( ');
+// }
+
+// else {
+//     console.log('your time is write :( ');
+// }
+
+
+//5.
+// var x = prompt('Input x: ');
+// var y = prompt('Input y: ');
+
+// if (x > 0 && y > 0) {
+//     console.log('X and Y are locate first quarter. ');
+// }
+// else{
+//     console.log('X and Y are locate in second or third areas. ');
+// }
+
+// if (x == 0 && y == 0) {
+//     console.log('X and Y are locate in center of coordinate plane. ');
+// }
+
+
+//6.
+// const num = prompt('Input number of a month');
+
+// if (num > 12 || num < 0) {
+//     console.log('Incorrect input. ');
+// }
+
+// switch(num) {
+//     case '1':
+//         console.log('January');
+//         break;
+//     case '2':
+//         console.log('February');
+//         break;
+//     case '3':
+//         console.log('March');
+//         break;
+//     case '4':
+//         console.log('April');
+//         break;
+//     case '5':
+//         console.log('May');
+//         break;
+//     case '6':
+//         console.log('June');
+//         break;
+//     case '7':
+//         console.log('July');
+//         break;
+//     case '8':
+//         console.log('August');
+//         break;
+//     case '9':
+//         console.log('September');
+//         break;
+//     case '10':
+//         console.log('October');
+//         break;
+//     case '11':
+//         console.log('November');
+//         break;
+//     case '12':
+//         console.log('December');
+//         break;
+// }
+
+
+//7.
+// var num1 = prompt('Input number: ');
+// var num2 = prompt('Input number again: ');
+
+// num1 > num2 ? console.log('Num1 more than Num2. ') : console.log('Num1 less than Num2. ');
+
+
+//8.
+// var num  = prompt('Input number: ');
+
+// num1 % 5 ? console.log('Num1 кратно 5. ') : console.log('Num1 не кратно 5. ');
+
+
+//9.
+
+// var num = prompt('Inpunt planet: ');
+
